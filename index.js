3665b6b4d73adcdedf434dad94e6307ca9c8ed29
José Miranda <josemiranda989@gmail.com>
2021-11-23 13:09:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+$(window).load(function() {
+    $(".loader").fadeOut("slow");
+});
+
+
+
+// A $( document ).ready() block.
+$(document).ready(function() {    
+$('.top a').click(function(e){
+        e.preventDefault();
+        var offset = $($(this).attr('href')).offset().top;
+        $('html, body').animate({scrollTop:offset}, 600);
+        return false;
+    });
+
+});
